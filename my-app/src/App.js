c336e31e315efd3531b648fa0de029eea1a30474
maryna.shyta <maryna.shyta@gmail.com>
2022-12-01 12:10:23 +0200

    revised work with arrays

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,83 +1,17 @@
 import React, { useState } from "react";
+import PostList from "./components/PostList";
 
+const App = () => {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "PHP", body: "Description" },
+    { id: 2, title: "JavaScript", body: "Description" },
+  ]);
 
-const ProductCategoryRow = (props) => {
-    const category = useState(props.category);
-    return (
-      <tr>
-        <th colSpan="2">
-          {category}
-        </th>
-      </tr>
-    );
-}
-
-const ProductRow = (props) => {
-    const product = useState(props.product);
-    const name = props.product.stocked ? props.product.name : <span style={{ color : 'red' }}>{props.product.name}</span>;
-
-    return (
-      <tr>
-        <td>{name}</td>
-        <td>{props.product.price}</td>
-      </tr>
-    );
-}
-
-const ProductTable = (props) => {
-    const rows = useState([]);
-    let lastCategory = null;
-    
-    props.products.forEach((product) => {
-      if (product.category !== lastCategory) {
-        rows.push(
-          <ProductCategoryRow
-           category={product.category}
-           key={product.category} />
-        );
-      }
-      rows.push(
-        <ProductRow
-        product={product}
-        key={product.name} />
-      );
-      lastCategory = product.category;
-    });
-
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    );
-}
-
-const SearchBar = () => {
-    return (
-      <form>
-        <input type="text" placeholder="Search..." />
-        <p>
-          <input type="checkbox" />
-          {' '}
-          Only show products in stock
-        </p>
-      </form>
-    );
-}
-
-const FilterableProductTable = (props) => {
   return (
-    <div>
-      <SearchBar />
-      <ProductTable products={props.products} />
+    <div className="App">
+      <PostList posts={posts} />
     </div>
   );
-}
+};
 
-
-export default FilterableProductTable;
+export default App;
---
+++ my-app/src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const PostItem = (props) => {
+  return (
+    <div className="App">
+      <div className="post">
+        <div className="post__content">
+          <strong>{props.post.title}</strong>
+          <div>{props.post.body}</div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
