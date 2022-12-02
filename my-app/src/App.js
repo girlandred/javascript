9f248d038752efca01994fc16fc9c3835198bc22
maryna.shyta <maryna.shyta@gmail.com>
2022-12-02 10:39:17 +0200

    useMemo hook implementation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 
 const App = () => {
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+
   const [posts, setPosts] = useState([
     { id: 1, title: "PHP", body: "Description" },
     { id: 2, title: "JavaScript", body: "Description" },
   ]);
+
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
+    }
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortedSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -19,7 +37,17 @@ const App = () => {
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={deletePost} posts={posts} title="All posts" />
+      <div style={{ padding: "15px 0" }}></div>
+      <PostFilter filter={filter} setFilter={setFilter}/>
+      {sortedSearchedPosts.length !== 0 ? (
+        <PostList
+          remove={deletePost}
+          posts={sortedSearchedPosts}
+          title="All posts"
+        />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Posts not found</h1>
+      )}
     </div>
   );
 };
